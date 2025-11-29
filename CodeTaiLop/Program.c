db992e424a2bb88a4a123d858b720b93427fe7fc
PH64953 <nguyentuanlinh19012004@gmail.com>
2025-11-29 10:52:51 +0700

    Sap xep mang theo thu tu

---
+++ CodeTaiLop/Program.c
@@ -0,0 +1,98 @@
+
+#include <stdio.h>
+
+void kiemTraSoNguyen()
+{
+	printf("Kiem Tra So Nguyen");
+}
+
+void uocChungBoiChung()
+{
+	printf("Uoc Chung Boi Chung");
+}
+
+void sapXepPhanTuMang1Chieu()
+{
+	int integerArray[100];
+	int tmp;
+	int length;
+	printf("Nhap so luong phan tu mang: ");
+	scanf("%d", &length);
+	printf("Nhap du lieu mang %d phan tu:\n", length);
+	for (int i = 0; i < length; i++)
+	{
+		printf("Phan tu thu %d: ", i + 1);
+		scanf("%d", &integerArray[i]);
+	}
+	printf("Xuat mang du lieu %d phan tu Giam dan\n", length);
+	for (int i = length - 1; i > -1; i--)
+	{
+		printf("mang[%d] = %d\n", i, integerArray[i]);
+	}
+	printf("Sap xep tang dan du lieu mang %d phan tu\n", length);
+	for (int i = 0; i < length - 1; i++)
+	{
+		if (integerArray[i] > integerArray[i + 1])
+		{
+			tmp = integerArray[i];
+			integerArray[i] = integerArray[i + 1];
+			integerArray[i + 1] = tmp;
+			i = -1; // reset i to -1 because it will be incremented to 0 at the start of the next loop iteration
+		}
+	}
+	printf("In du lieu mang %d phan tu\n", length);
+	for (int i = 0; i < length; i++)
+	{
+		printf("mang[%d] = %d\n", i, integerArray[i]);
+	}
+}
+void lapChucNang(int chonChucNang)
+{
+	int tiepTuc = 1;
+	while (tiepTuc == 1)
+	{
+		switch (chonChucNang)
+		{
+		case 1:
+			kiemTraSoNguyen();
+			break;
+		case 2:
+			uocChungBoiChung();
+			break;
+		case 3:
+			sapXepPhanTuMang1Chieu();
+			break;
+		case 0:
+			return;
+		default:
+			printf("Hay chon lai [0-3]");
+			break;
+		}
+
+		printf("\n");
+		printf("Tiep tuc CN nay? [1=Co | 0=Khong]: ");
+		scanf("%d", &tiepTuc);
+		system("cls");
+	}
+}
+
+int main()
+{
+	int chonChucNang;
+	do
+	{
+		printf("Menu");
+		printf("\n");
+		printf("1. Kiem Tra So Nguyen");
+		printf("\n");
+		printf("2. Uoc Chung Boi Chung");
+		printf("\n");
+		printf("3. Sap xep mang");
+		printf("\n");
+		printf("Hay chon CN [0-3]: ");
+		scanf("%d", &chonChucNang);
+		lapChucNang(chonChucNang);
+	} while (chonChucNang != 0);
+}
+
+// GV: AnhTT184
